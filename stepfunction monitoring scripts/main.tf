1dbdc11412a103cdea74956a5f9417cfa3c2a8a9
krishnaduttPanchagnula <krishnadutt123@gmail.com>
2022-02-02 15:08:00 +0530

    ches to main file

--- stepfunction monitoring scripts/main.tf
+++ stepfunction monitoring scripts/main.tf
@@ -9,13 +9,13 @@ terraform {
 
 # Configure the AWS Provider region
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
- # step and lambda function - failed or other problems
+# step function - failed or other problems - monitoring code
 resource "aws_cloudwatch_event_rule" "console" {
-  name        = "captureawsactivity"
-  
+  name = "captureawsactivity"
+
 
   event_pattern = <<EOF
 {
@@ -29,31 +29,105 @@ resource "aws_cloudwatch_event_rule" "console" {
 EOF
 }
 
+
+#AWS cloudwatch event rule
 resource "aws_cloudwatch_event_target" "sns" {
   rule      = aws_cloudwatch_event_rule.console.name
   target_id = "SendToSNS"
   arn       = aws_sns_topic.aws_activity.arn
 }
 
-# Below is fine
+#lambda policy creation function
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+
+#lambda monitoring code
+resource "aws_lambda_function_event_invoke_config" "example" {
+
+  function_name = "arn:aws:lambda:ap-south-1:498830417177:function:AAA"
+
+  destination_config {
+    
+    # on_failure {
+    #   destination = aws_sns_topic.aws_activity.arn
+    # }
+
+    on_success {
+      destination = aws_sns_topic.aws_activity.arn
+    }
+
+
+  }
+}
+#  """hook the path of the python zip here to the one preset in the below code"""
+# locals {
+#   lambda_zip_location = "lambdas\s3_upload.zip"
+# }
+
+
+# """Work needed to make sure the lambda works and shows errors when there is a problem"""
+#lambda creation function
+# resource "aws_lambda_function" "test_lambda" {
+#   filename      = "lambda_function_payload.zip"
+#   function_name = "lambda_function_name"
+#   role          = aws_iam_role.iam_for_lambda.arn
+#   handler       = "index.test"
+
+ 
+#   runtime = "python3.8"
+
+#   environment {
+#     variables = {
+#       foo = "bar"
+#     }
+#   }
+# }
+
+
+
+# SNS topic creation
 resource "aws_sns_topic" "aws_activity" {
   name = "aws-console-activity"
 }
 
+# SNS topic subscription
+
 resource "aws_sns_topic_subscription" "email-target" {
   topic_arn = aws_sns_topic.aws_activity.arn
   protocol  = "email"
-  endpoint  = "johnaws1209@gmail.com"
+  endpoint  = var.email
 }
 
+#SNS topic policy
 resource "aws_sns_topic_policy" "default" {
-  arn    = "${aws_sns_topic.aws_activity.arn}"
-  policy = "${data.aws_iam_policy_document.sns_topic_policy.json}"
+  arn    = aws_sns_topic.aws_activity.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
 }
 
+
+#SNS IAM policy
 data "aws_iam_policy_document" "sns_topic_policy" {
   statement {
-    effect  = "Allow"
+    effect = "Allow"
     actions = [
       "SNS:Subscribe",
       "SNS:SetTopicAttributes",
