a7003e52b9e6fb8f87c865980efc32e95843be89
Krishna Dutt Panchagnula <krishnadutt123@gmail.com>
2022-03-14 19:26:34 +0530

    updated variables for security

--- Monitor_all_aws_infrastructure_with sns email/variables.tf
+++ Monitor_all_aws_infrastructure_with sns email/variables.tf
@@ -2,19 +2,17 @@ variable "region" {
   description = "The id of the region"
   type        = string
 
-  default = "498830417177"
-
-
+  default = "Your-region-id"
 }
 
 variable "email" {
   description = "Email to send sns notification"
   type        = string
-  default     = "johnaws1209@gmail.com"
+  default     = "youremail@yourdomain.com"
 
 }
 variable "account" {
   description = "account name"
   type        = string
-  default     = "498830417177"
+  default     = "your-account-number"
 }
